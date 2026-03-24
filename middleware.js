d4dab11d8d0d4c86865dc9a6703e837e3f4e935d
Masaki.Sawada <sawada0517@gmail.com>
2026-03-24 16:40:58 +0900

    Add Basic auth for STG

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === 'sketto' && pwd === 'stg2026') {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
